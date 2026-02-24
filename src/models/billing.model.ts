22fde5cc1386e94e870dac17e0930346290c8d84
Dinh Truong <kimtrietvan@gmail.com>
2026-02-24 15:45:03 +0700

    fix name billing

--- src/models/billing.model.ts
+++ src/models/billing.model.ts
@@ -2,7 +2,7 @@ import {Schema, model, Types} from 'mongoose';
 
 const BillingSchema = new Schema({
     _id: {type: String, required: true, default: () => new Types.ObjectId().toString() },
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true, default: '' },
     location: { type: String, default: '' },
     taxNumber: { type: String, default: '' },
 }, { timestamps: true });
