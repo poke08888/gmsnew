e832a0badaa3d75888b0ab9a601d6318dee00fbc
Dinh Truong <kimtrietvan@gmail.com>
2026-02-24 15:02:21 +0700

    fix warehouse name

--- src/models/warehouse.model.ts
+++ src/models/warehouse.model.ts
@@ -2,7 +2,7 @@ import {Schema, model, Types} from 'mongoose';
 
 const WarehouseSchema = new Schema({
     _id: {type: String, required: true, default: () => new Types.ObjectId().toString() },
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true, default: '' },
     location: { type: String, default: '' },
     contactName: { type: String, default: '' },
     contactPhone: { type: String, default: '' },
